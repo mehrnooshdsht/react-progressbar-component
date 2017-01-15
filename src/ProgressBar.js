1290b5665a5bad3a01ff9cb623ae39370eae2295
mehrnooshdsht <mehrnooshdsht@gmail.com>
2017-01-15 17:15:43 +0330

    Add initial progressbar component
    -add progressbar js 
    add progressbar scss

---
+++ src/ProgressBar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import styles from './ProgressBar.scss';
+
+export default class ProgressBar extends Component {
+
+  state ={
+    isClicked: false,
+    offset: 0 //use to set the left of draggable and width of progress
+  };
+
+  setBar () {
+    const {offset} = this.state;
+    this.draggable.style.left = `${offset-1}px`;//for better view
+    this.progress.style.width = `${offset}px`;
+  };
+
+  mouseDownHandler () {
+    event.preventDefault();
+    this.setState({
+      isClicked : true
+    });
+  }
+
+  mouseUpHandler () {
+    event.preventDefault();
+    this.setState({
+      isClicked : false
+    });
+  }
+
+  mouseMoveHandler (e){
+    if(this.state.isClicked === true){
+      const parentPosition = this.bar.offsetLeft;
+      const parentWidth = this.bar.getBoundingClientRect().width;
+      e = e || window.event;
+      let dragX = e.pageX;
+      this.setState({
+        offset : (dragX - parentPosition) <= 300 ? dragX - parentPosition : parentWidth
+      });
+      this.setBar();
+    }
+  }
+
+  render(){
+    return(
+      <div id="bar" ref={(div) => { this.bar = div; }} onMouseDown={this.mouseDownHandler.bind(this)} onMouseUp={this.mouseUpHandler.bind(this)} onMouseLeave={this.mouseUpHandler.bind(this)} onMouseMove={this.mouseMoveHandler.bind(this)}>
+        <div id="progress" ref={(div) => { this.progress = div; }}>
+        </div>
+        <div id="draggable" ref={(div) => { this.draggable = div; }}>
+        </div>
+      </div>
+      )
+  }
+}
